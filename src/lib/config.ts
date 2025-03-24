85ac8f38be65450131bdb0dc39db36851b2bf98d
Matt Safaii <matt@mgency.com>
2025-03-24 02:25:45 -0700

    sveltekit markdown blog

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Matt Safaii';
+export const description = 'Building things with code';
+export const url = dev ? 'http://localhost:5173/' : 'https://mattsafaii.com/';
